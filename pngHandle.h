8795f5895e03cab36962a1d87d95c2dc5b350fb4
阿振 <zhenli_ysu@126.com>
2018-10-27 14:45:14 +0800

    Update pngHandle.h

--- pngHandle.h
+++ pngHandle.h
@@ -13,23 +13,28 @@ public:
     ~pngHandle();
     pngHandle();
     pngHandle(const int width,const int height);
-
+    //设置图片大小
     void setSize(const int width,const int height);
+    //设置图片背景
     bool setBackground(unsigned char value);
+    //设置像素值
     bool setValue(int x,int y,unsigned char value);
+    //图片写出
     bool write(char * filename);
 
 
 private:
 
 
-    bool isSetSize;
+    bool isSetSize;//图片大小是否已经设置标志
     char bit_depth_;//图像深度
-    char color_type_;
-    int width_; //图寬
+    char color_type_;//颜色类型
+    int width_; //图宽
     int height_;//图高
 
+    //设置图像深度，功能暂不开放
     void setBitDepth(char bit_depth);
+    //设置颜色类型，功能暂不开放
     void setColortype(char color_type);
 
     unsigned char *buffer;// 0 <= gray < 255
