9164dbf546744a3ee1454383f6324a5423f4a4e2
阿振 <zhenli_ysu@126.com>
2018-10-29 09:19:38 +0800

    Update pngHandle.cpp

--- pngHandle.cpp
+++ pngHandle.cpp
@@ -119,6 +119,5 @@ bool pngHandle::write(const char *filename) {
     //清空内存
     if (info_ptr != NULL) png_free_data(png_ptr, info_ptr, PNG_FREE_ALL, -1);
     if (png_ptr != NULL) png_destroy_write_struct(&png_ptr, (png_infopp)NULL);
-    if (row_pointers != NULL) free(row_pointers);
     if (fp != NULL) fclose(fp);
 }
